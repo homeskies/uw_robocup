e2484769c8a6845d2aec2ceff710b0bda5836290
Nick Walker <nswalker@cs.washington.edu>
2020-02-15 08:33:58 -0800

    Fix lint issues
    Remove inspection Python module
    Not needed because shared code should probably live in common_states
    Fix catkin lint issues
    Add roslint to groceries package

--- inspection/setup.py
+++ inspection/setup.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python
+# ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(
-    packages=['inspection'],
-    package_dir={'': 'src'},
-)
+# No module in this package, we just need this
+# so roslint can actually attach to the catkin python hooks
+setup_args = generate_distutils_setup(
+    packages=[],
+    package_dir={})
 
-setup(**d)
+setup(**setup_args)
