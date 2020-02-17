dd00a65e398561bcd1381fbb92f4cc4effaed52e
Nick Walker <nswalker@cs.washington.edu>
2020-02-17 12:19:32 -0800

    Add poi_name input to go_to_named_poi

--- common_states/states/common_states/go_to_named_poi/go_to_named_poi_HGLHWO/script.py
+++ common_states/states/common_states/go_to_named_poi/go_to_named_poi_HGLHWO/script.py
@@ -2,8 +2,12 @@ def execute(self, inputs, outputs, gvm):
     base, _ = gvm.get_variable("robot")
     ltmc = gvm.get_variable("knowledgebase")
     map = ltmc.get_map("arena")
-    pose = map.get_pose(inputs["name"])
-    base.navigate_to(pose)
+    pose = map.get_pose(inputs["poi_name"])
+    if not pose:
+        self.logger.error("No pose named {}".format(inputs["poi_name"]))
+        return "aborted"
+    base.navigate_to(pose.x, pose.y, pose.theta)
+    self.logger.info("Moving to {} {}".format(pose.x, pose.y))
     result = base.wait_for_navigation_result()
-    self.loginfo(result)
+    self.logger.info(result)
     return "success"
