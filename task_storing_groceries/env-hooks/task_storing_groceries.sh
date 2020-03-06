ead60b2f7c168e0d384d9c376a66dc0672016432
edansneh <edan.sneh@gmail.com>
2020-03-05 16:10:23 -0800

    fixes RAFCON_LIBRARY_PATH
    this env variable was copying multiple times every time you source it
    and was just really ugly

--- task_storing_groceries/env-hooks/task_storing_groceries.sh
+++ task_storing_groceries/env-hooks/task_storing_groceries.sh
@@ -1,1 +1,3 @@
+if [ "$RAFCON_LIBRARY_PATH" != *$(rospack find task_storing_groceries)/states/task_storing_groceries* ]; then
 export RAFCON_LIBRARY_PATH=$(rospack find task_storing_groceries)/states/task_storing_groceries:$RAFCON_LIBRARY_PATH
+fi
