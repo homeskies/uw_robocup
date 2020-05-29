05f087153c4967fdf2a13df271a40edf3d37fda0
Nick Walker <nswalker@cs.washington.edu>
2020-05-29 16:00:37 -0500

    Update RAFCON PATH insertion conditions
    Previous condition wasn't working in my environment, but this recommendation did
    https://unix.stackexchange.com/questions/217622/add-path-to-path-if-not-already-in-path
    Use double bracket in conditional to avoid emitting error
    Quote strings to handle unusual characters in variables

--- inspection/env-hooks/inspection.sh
+++ inspection/env-hooks/inspection.sh
@@ -1,3 +1,3 @@
-if [ "$RAFCON_LIBRARY_PATH" != *$(rospack find inspection)/states/inspection* ]; then
-export RAFCON_LIBRARY_PATH=$(rospack find inspection)/states/inspection:$RAFCON_LIBRARY_PATH
+if [[ ":$RAFCON_LIBRARY_PATH:" != *":$(rospack find inspection)/states/inspection:"* ]]; then
+  export RAFCON_LIBRARY_PATH="$(rospack find inspection)/states/inspection:$RAFCON_LIBRARY_PATH"
 fi
